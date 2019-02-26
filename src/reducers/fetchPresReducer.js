7c3d8cfea61ff31aa2a862b871d75ce1573b23df
Travis Gee <geet084@gmail.com>
2019-02-26 10:12:44 -0700

    Add fetch reducer for presidents

--- src/reducers/fetchPresReducer.js
+++ src/reducers/fetchPresReducer.js
@@ -1,7 +1,6 @@
 export const fetchPresReducer = (state = [], action) => {
   switch (action.type) {
     case 'FETCH_PRES_SUCCESS':
-      console.log(action)
       return action.presidents;
     default:
       return state;
