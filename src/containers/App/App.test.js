df623d63917fffe059e151443bd56de32cf5f2ec
Travis Gee <geet084@gmail.com>
2019-02-26 11:32:34 -0700

    Add mapStateToProps test

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mapStateToProps, mapDispatchToProps } from './App';
+import { fetchPres } from '../../thunks/fetchPres'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('mapDispatchToProps', () => {
+  it.skip('calls dispatch with a url to fetch presidents', () => {
+    const mockUrl = 'http://localhost:9999'
+    const mockDispatch = jest.fn();
+    const actionToDispatch = fetchPres(mockUrl)
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.fetchPres(mockUrl);
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+  })
+})
+
+describe('mapStateToProps', () => {
+  it('should return an object with a presidents array, isLoading and hasErrored statuses', () => {
+    const mockState = {
+      presidents: [{ number: 1 }, { number: 2 }],
+      hasErrored: '',
+      isLoading: false,
+      fakeState: 'other things'
+    }
+    const expected = {
+      presidents: [{ number: 1 }, { number: 2 }],
+      hasErrored: '',
+      isLoading: false,
+    }
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(expected)
+  })
+})
